0ec1f0a8bb14ee388015f730dfe7655168260cbf
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-01-05 01:21:44 +0530

    2D ARRAYS

--- DAY10/TWO_ARRAY_PREFIX_SUM_Sum_of_given_coordinates.java
+++ DAY10/TWO_ARRAY_PREFIX_SUM_Sum_of_given_coordinates.java
@@ -6,12 +6,45 @@ public class TWO_ARRAY_PREFIX_SUM_Sum_of_given_coordinates {
     public static void main(String[] args) {
         int[][] matrix = {{1,2,3},{4,1,1},{1,1,1}};
 //        findprefixSum();
-        System.out.println(coordRecSum(matrix,0,0,2,2));
+        System.out.println(coordRectanSum1(matrix,0,0,2,2));
         System.out.println(coordRectanSum2(matrix,0,0,2,2));
+        System.out.println(coordRectanSum3(matrix,0,0,2,2));
 
     }
 
-    public static int coordRecSum(int[][] matrix,int r1,int l1,int r2, int l2){
+    public static void findprefixSum2(int[][] matrix) {
+
+        int r = matrix.length;
+        int c = matrix[0].length;
+        //Traverse horizontally to find row-wise prefix sum
+        for (int i = 0; i < r; i++) {
+            for (int j = 1; j < c; j++) {
+                matrix[i][j] += matrix[i][j - 1];
+            }
+        }
+    }
+
+    public static void findprefixSum3(int[][] matrix){
+        //calculate row-wise and column wise sum
+        //matrix[i][j] = sumRectangle((0,0) to (i,j) coordinates)
+
+        int r = matrix.length;
+        int c = matrix[0].length;
+        //Traverse horizontally to find row-wise prefix sum
+        for(int i=0 ; i<r ; i++){
+            for(int j=1 ; j<c ; j++){
+                matrix[i][j] += matrix[i][j-1];
+            }
+        }
+        //traverse vertically to find col-wise prefix sum
+        for(int j=0 ; j<c ; j++){
+            for(int i=1 ; i<r ; i++){
+                matrix[i][j] +=matrix[i-1][j];
+            }
+        }
+    }
+
+    public static int coordRectanSum1(int[][] matrix,int r1,int l1,int r2, int l2){
         int sum=0;
         for(int i=l1 ; i<=l2 ; i++){
             for(int j=r1 ; j<=r2 ; j++){
@@ -20,19 +53,10 @@ public class TWO_ARRAY_PREFIX_SUM_Sum_of_given_coordinates {
         }
         return sum;
     }
-    public static void findprefixSum(int[][] matrix){
-        int r = matrix.length;
-        int c = matrix[0].length;
-        for(int i=0 ; i<r ; i++){
-            for(int j=1 ; j<c ; j++){
-                matrix[i][j] += matrix[i][j-1];
-            }
-        }
-    }
 
     public static int coordRectanSum2(int[][] matrix,int r1,int l1,int r2, int l2){
         int sum=0;
-        findprefixSum(matrix);
+        findprefixSum2(matrix);
 
         ///l1 l2 are rows
         for(int i=l1 ; i<=l2 ; i++){
@@ -42,6 +66,24 @@ public class TWO_ARRAY_PREFIX_SUM_Sum_of_given_coordinates {
                 sum+=matrix[i][r2];
         }
         return sum;
+    }
+
+    public static int coordRectanSum3(int[][] matrix,int r1, int l1, int r2, int l2){
+        int ans = 0, sum=0 ,up = 0, left = 0, upleft = 0;
+        findprefixSum3(matrix);
+
+        sum = matrix[l2][r2];
+        if(r1>=1) {
+            left = matrix[l2][r1 - 1];
+        }
+        if(l1>=1) {
+            up = matrix[l1 - 1][r2];
+        }
+        if(l1>=1 && r1>=1) {
+            upleft = matrix[l1 - 1][r1 - 1];
+        }
+        ans  = sum - up - left + upleft;
+        return ans;
     }
 
 }
