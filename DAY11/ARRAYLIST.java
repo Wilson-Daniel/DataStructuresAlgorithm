b68fed9e4cc1a132abe34c02491e82d4a642e7b8
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-01-05 01:22:19 +0530

    ARRAYLIST

---
+++ DAY11/ARRAYLIST.java
@@ -0,0 +1,13 @@
+package DataStructuresAlgorithm.DAY11;
+
+public class ARRAYLIST {
+    public static void main(String[] args) {
+
+        //WRAPPER Class to deal with packages taking only objects
+        Integer i = Integer.valueOf(4);
+        System.out.println(i);
+
+        Float f = Float.valueOf(4.5f);
+        System.out.println(f);
+    }
+}
