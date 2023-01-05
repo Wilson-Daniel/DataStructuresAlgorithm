5374c9ac1ef9ee2721a3b0fdc41dbcaf20106d46
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-01-05 23:54:09 +0530

    ARRAYLIST

--- DAY11/ARRAYLIST.java
+++ DAY11/ARRAYLIST.java
@@ -1,5 +1,7 @@
 package DataStructuresAlgorithm.DAY11;
 
+import java.util.Scanner;
+
 public class ARRAYLIST {
     public static void main(String[] args) {
 
@@ -9,5 +11,45 @@ public class ARRAYLIST {
 
         Float f = Float.valueOf(4.5f);
         System.out.println(f);
+//        int n;
+//        System.out.print("Enter the number :");
+//        Scanner sc = new Scanner(System.in);
+//        n = sc.nextInt();
+//        int character = 65;
+//        int storeValue = 0;
+//        int character2 = 0;
+//
+//        for(int i=1;i<=n;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print((char)character+" ");
+//                character++;
+//            }
+//            character--;
+//            storeValue = character;
+//
+//            for(int j=1;j<=(n-i)*2;j++){
+//                System.out.print("  ");
+//            }
+//            for(int j=1;j<=i;j++){
+//                System.out.print((char)character+" ");
+//                character--;
+//            }
+//            character = 65;
+//            System.out.println();
+//        }
+//        character2 = storeValue;
+//        for(int i=1;i<=n;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=n;j>=i;j--){
+//                System.out.print((char)character2+" ");
+//                character2--;
+//            }
+//
+//
+//            character2 = storeValue-1;
+//            System.out.println();
+//        }
     }
 }
