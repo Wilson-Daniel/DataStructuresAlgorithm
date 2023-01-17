46855fb87e17675261bf554514c3ff3d89d880eb
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-01-17 22:36:25 +0530

    RECURSION

---
+++ DAY16/RECURSION_Print_K_multiples.java
@@ -0,0 +1,15 @@
+package DataStructuresAlgorithm.DAY16;
+
+public class RECURSION_Print_K_multiples {
+    public static void main(String[] args) {
+        printK(12,5);
+    }
+    public static void printK(int n, int k){
+        if(k==0){
+            return;
+        }
+        printK(n,k-1);
+        System.out.print(n*k+" ";
+
+    }
+}
