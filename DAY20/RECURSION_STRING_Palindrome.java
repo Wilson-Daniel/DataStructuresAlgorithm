d36be38006b996d475b2b7afd7577376871c8b40
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-01-28 23:01:36 +0530

    Recursion

--- DAY20/RECURSION_STRING_Palindrome.java
+++ DAY20/RECURSION_STRING_Palindrome.java
@@ -2,15 +2,23 @@ package DataStructuresAlgorithm.DAY20;
 
 public class RECURSION_STRING_Palindrome {
     public static void main(String[] args) {
-        String s = "a";
-        if(s.equals(palindrome(s))){
-            System.out.println("Yes");
-        }else{
-            System.out.println("No");
-        }
+        String s = "abbas";
+//        if(s.equals(palindrome(s,0,5))){
+//            System.out.println("Yes");
+//        }else{
+//            System.out.println("No");
+//        }
+        System.out.println(palindrome(s,0,s.length()));
+//        int l=0,r=4;
+////        System.out.println(s.substring(l+1,r-1));
+//        String c = "abcde";
+//        System.out.println(reverseStr(c));
     }
-    public static String palindrome(String s){
-        return reverseStr(s);
+    public static boolean palindrome(String s,int l,int r){
+        if(l<=r){
+            return true;
+        }
+        return (s.charAt(l) == s.charAt(r)) && palindrome(s,l+1,r-1);
     }
     public static String reverseStr(String s){
         if(s.length()==0){
