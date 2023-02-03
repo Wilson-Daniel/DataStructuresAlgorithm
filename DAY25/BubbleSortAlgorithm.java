ea05b64bdafc93590808dfe4905f3747ce9a9b62
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-02-04 00:38:12 +0530

    Bubble sort optimized

--- DAY25/BubbleSortAlgorithm.java
+++ DAY25/BubbleSortAlgorithm.java
@@ -3,13 +3,23 @@ package DataStructuresAlgorithm.DAY25;
 public class BubbleSortAlgorithm {
     public static void bubbleSort(int[] arr){
         int n = arr.length;
-        for(int i=0 ; i<n ; i++){
+        for(int i=0 ; i<n-1 ; i++){
+            // n-1 iterations
+            boolean flag = false; //has any swap happened, optimizing the code for nearly sorted array 
             for(int j=0 ; j<n-i-1 ; j++){
+                /*
+                last i element are already at correct sorted position,
+                so no need to check them
+                 */
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    flag = true;
                 }
+            }
+            if(flag == false){ // have any swap happened
+                return;
             }
         }
     }
