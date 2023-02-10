8df917c92bd07243dfecb36321811b124e81b7b2
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-02-11 01:06:31 +0530

    Selection Sort

--- DAY26/Selection_Sort.java
+++ DAY26/Selection_Sort.java
@@ -10,13 +10,15 @@ public class Selection_Sort {
 
     }
     public static void selectionSort(int[] arr,int n){
-        for(int i=0 ; i<n ; i++){
+        for(int i=0 ; i<n-1 ; i++){
+            int min_ind = i;
             for(int j=i+1 ; j<n ; j++){
                 if(arr[i] > arr[j]){
-                    int temp = arr[i];
-                    arr[i]  = arr[j];
-                    arr[j] = temp;
+                    min_ind = j;
                 }
+                int temp = arr[i];
+                arr[i]  = arr[min_ind];
+                arr[min_ind] = temp;
             }
         }
     }
