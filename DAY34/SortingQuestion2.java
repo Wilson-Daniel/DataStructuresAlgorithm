d4cf169cf55f1b8a6ce488bb1a3cfc086c7edce0
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-02-21 23:40:54 +0530

    Sorting Questions

--- DAY34/SortingQuestion2.java
+++ DAY34/SortingQuestion2.java
@@ -1,13 +1,37 @@
 package DataStructuresAlgorithm.DAY34;
 
 public class SortingQuestion2 {
+    /*
+    Given an array of positive and negative integers, segregate them in linear time and constant space.
+    The output should print all negative numbers, followed by all positive numbers.
+       Input: [19,-20, 7, -4, -13, 11, -5,3]
+       Output: [-20 -4 -13 -5 7 11 19 3 ]
+     */
     public static void arrange(int[] arr){
         int left=0;
         int right = arr.length-1;
         while(left<right){
-            while(){
-
+            while(arr[left]<0){
+                left++;
             }
+            while(arr[right]>=0){
+                right--;
+            }
+            if(left < right){
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-1,3,-3,4,2,56,-45};
+        arrange(arr);
+        for(int i : arr){
+            System.out.print(i+" ");
         }
     }
 }
---
+++ DAY34/SortingQuestion3.java
@@ -0,0 +1,41 @@
+package DataStructuresAlgorithm.DAY34;
+
+public class SortingQuestion3 {
+    /*
+    Given an array of size N containing only Os, 1s, and 2s; sort the array in ascending order.
+        Input:  N=6
+                arr[]= {0 2 1 2 0 0}
+        Output: 000122
+     */
+    public static void swap(int[] arr,int x,int y){
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+    public static void sort012(int[] a){
+        int l = 0, mid =0, h = a.length-1;
+        
+        //exploring the unknown region
+        while(l<=h){
+            if(a[mid] == 0){
+                swap(a,mid,l);
+                mid++;
+                l++;
+            }else if(a[mid] == 1){
+                mid++;
+            }else{
+                swap(a,mid,h);
+                h--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,0,0,0,1,1,2,2};
+        sort012(arr);
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+
+}
