eb2345bbac403bc31316a2b65991a1003e8d824b
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-02-26 23:59:47 +0530

    ds

--- DAY37/Ceiling_in_a_sorted_array_DSASHEET.java
+++ DAY37/Ceiling_in_a_sorted_array_DSASHEET.java
@@ -2,6 +2,6 @@ package DataStructuresAlgorithm.DAY37;
 
 public class Ceiling_in_a_sorted_array_DSASHEET {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ DAY38/BinarySearch.java
@@ -0,0 +1,41 @@
+package DataStructuresAlgorithm.DAY38;
+
+public class BinarySearch {
+
+    public static boolean binarySearch(int[] arr,int target){
+        int n= arr.length;
+        int st=0,end=n-1;
+        while(st<=end){
+            int mid = (st+end)/2;
+            if(arr[mid] == target){
+                return true;
+            }else if(target<arr[mid]){
+                end = mid-1;
+            }else{
+                st = mid+1;
+            }
+        }
+        return false;
+    }
+    public static boolean recursibeBinary(int[] arr,int target,int st,int end){
+        if(st>end) return false; // base case
+
+        int mid = (st+end)/2;
+        if(target == arr[mid]){
+            return true;
+        }else if(target < arr[mid]){
+            return recursibeBinary(arr,target,st,mid-1);
+        }else{
+            return recursibeBinary(arr,target,mid+1,end);
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int target = 0;
+        while(target != 10){
+            System.out.printf("%d exists in arr: %b \n", target, recursibeBinary(arr,target,0,arr.length-1));
+            target++;
+        }
+    }
+}
