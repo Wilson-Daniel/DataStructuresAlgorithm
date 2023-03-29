5aeb2e853592851aa5149729cff750efe2ef111e
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-03-29 23:23:12 +0530

    LL

--- DAY_48/LL.java
+++ DAY_48/LL.java
@@ -1,6 +1,9 @@
 package DataStructuresAlgorithm.DAY_48;
+
+
+
+
 import java.util.LinkedList;
-import DataStructuresAlgorithm.DAY1.Node;
 
 public class LL {
     static Node head;
@@ -12,17 +15,77 @@ public class LL {
             this.next = null;
         }
     }
+    private Node findNode(Node head){
+        Node slow = head;
+        Node fast = head.next;
+        while(head!=null && head.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
     public static void main(String[] args){
 
-        head = new Node(1);
-        Node temp  = new Node(2);
-        head.next = temp;
-        head.next.next = new Node(3);
-        head.next.next.next = temp;
-        System.out.println(isCycle());
-        removeCycle();
-        System.out.println(isCycle());
+//        head = new Node(1);
+//        Node temp  = new Node(2);
+//        head.next = temp;
+//        head.next.next = new Node(3);
+//        head.next.next.next = temp;
+//        System.out.println(isCycle());
+//        removeCycle();
+//        System.out.println(isCycle());
+        LinkedList ll = new LinkedList();
+        ll.addFirst(5);
+        ll.addFirst(4);
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        System.out.println(ll);
+
     }
+    public Node mergeSort(Node head){
+        if(head == null || head.next==null){
+            return head;
+        }
+        //find Mid
+        Node mid = findNode(head);
+        //left and right Node
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+        //medge
+        return merge(newLeft,newRight);
+    }
+
+
+    public Node merge(Node head1, Node head2){
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+        while(head1!=null && head2!=null){
+            if(head1.data <= head2.data){
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            }else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while(head1!= null){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while(head2!=null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+
     public static boolean isCycle(){
         Node slow = head;
         Node fast = head;
