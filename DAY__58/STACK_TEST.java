68794d725cbf3a32579fde86c80d062cd57c73f9
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-04-10 02:01:36 +0530

    test

---
+++ DAY__58/STACK_TEST.java
@@ -0,0 +1,17 @@
+package DataStructuresAlgorithm.DAY__58;
+
+import java.util.Stack;
+
+public class STACK_TEST {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        s.add(4);
+        s.add(5);
+        for(int i=0 ; i<s.size() ; i++){
+
+        }
+    }
+}
