3989394057d6708cc4d7572e67849be130924366
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-04-14 23:18:39 +0530

    queue

--- DAY__61/FirstNon_Rep.java
+++ DAY__61/FirstNon_Rep.java
@@ -6,15 +6,25 @@ import java.util.Scanner;
 
 public class FirstNon_Rep {
     public static void main(String[] args) {
-        Queue<Character> q1 = new LinkedList<>();
         String str = "aabccxb";
-        int n = str.length();
-        int i=1;
-        char nonRep = str.charAt(0);
-        while(i<n){
-
-            q1.add(str.charAt(i));
-
+        printNon(str);
+    }
+    public static void printNon(String str){
+        int freq[]  =new int[26];
+        Queue<Character> q = new LinkedList<>();
+        for(int i=0 ; i<str.length() ; i++){
+            char ch = str.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+            while(!q.isEmpty() && freq[q.peek()-'a']>1){
+                q.remove();
+            }
+            if(q.isEmpty()){
+                System.out.println(-1+" ");
+            }else{
+                System.out.println(q.peek()+" ");
+            }
         }
+        System.out.println();
     }
 }
