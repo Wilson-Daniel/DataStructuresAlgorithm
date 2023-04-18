06480dfbefedb9e343cddd8334fea7a182deb890
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-04-18 20:36:50 +0530

    ALPHARecursion

--- DAY__65/FibonacciSeries.java
+++ DAY__65/FibonacciSeries.java
@@ -1,4 +1,16 @@
 package DataStructuresAlgorithm.DAY__65;
 
 public class FibonacciSeries {
+    public static void main(String[] args) {
+        System.out.println(fibo(6));
+    }
+    public static int fibo(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        int fnm1 = fibo(n-1);
+        int fnm2 = fibo(n-2);
+        int fn = fnm1+fnm2;
+        return fn;
+    }
 }
