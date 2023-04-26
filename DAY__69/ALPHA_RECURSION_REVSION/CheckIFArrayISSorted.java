9642a4afeffe514a48bdf06ed581490d89f9dc7f
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-04-26 11:44:32 +0530

    Recursion Revise

--- DAY__69/ALPHA_RECURSION_REVSION/CheckIFArrayISSorted.java
+++ DAY__69/ALPHA_RECURSION_REVSION/CheckIFArrayISSorted.java
@@ -2,12 +2,19 @@ package DataStructuresAlgorithm.DAY__69.ALPHA_RECURSION_REVSION;
 
 public class CheckIFArrayISSorted {
     public static void main(String[] args) {
-
+        int[] arr = {3,4,5,9,7};
+        System.out.println(sorted(arr,0));
     }
     public static boolean sorted(int[] arr,int idx){
-        if(idx == arr.length){
+        if(idx == arr.length-1){
             return true;
         }
-        
+
+        if(arr[idx]>arr[idx+1]){
+            return false;
+        }
+        return sorted(arr,idx+1);
+
+
     }
 }
