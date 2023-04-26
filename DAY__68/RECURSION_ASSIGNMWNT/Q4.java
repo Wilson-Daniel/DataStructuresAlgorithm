3f641212d280d86a9cfd2b6bb8704f72c342b5fd
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-04-26 23:54:09 +0530

    Recursion

--- DAY__68/RECURSION_ASSIGNMWNT/Q4.java
+++ DAY__68/RECURSION_ASSIGNMWNT/Q4.java
@@ -3,22 +3,16 @@ package DataStructuresAlgorithm.DAY__68.RECURSION_ASSIGNMWNT;
 public class Q4 {
     public static void main(String[] args) {
         String s = "abcab";
-        System.out.println(substring(s,0,0,0));
+        System.out.println(substring(s,0,s.length()-1,s.length()));
     }
-    public static int substring(String s,int idx, int lastIdx,int count){
-        if(s.length()==0){
+    public static int substring(String s,int idx, int lastIdx,int n){
+        if(n==1){
+            return 1;
+        }
+        if(n<=0){
             return 0;
         }
-        if(idx == s.length()){
-            return 0;
-        }
-
-        String small = s.substring(lastIdx,idx);
-        substring(s,idx+1,lastIdx,count);
-        if(small.charAt(lastIdx) == small.charAt(idx)){
-            count++;
-        }
-        substring(s.substring(1),idx+1,lastIdx+1,count);
+        int  count = substring(s,idx+1,lastIdx,n-1)+substring(s,idx,lastIdx-1,n-1) - substring(s,idx+1,lastIdx-1,n-2);
         return count;
     }
 }
