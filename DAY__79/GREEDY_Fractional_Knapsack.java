92117fcd350c52577899ca12212aab1a875f561d
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-05-13 01:41:35 +0530

    Greedy

---
+++ DAY__79/GREEDY_Fractional_Knapsack.java
@@ -0,0 +1,9 @@
+package DataStructuresAlgorithm.DAY__79;
+
+public class GREEDY_Fractional_Knapsack {
+    public static void main(String[] args) {
+        int[] value = {60,100,120};
+        int[] weight = {10,20,30};
+
+    }
+}
