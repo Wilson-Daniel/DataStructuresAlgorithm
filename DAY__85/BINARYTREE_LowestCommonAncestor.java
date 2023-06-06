2495fa4ee30999672162a0b1cf51e3df075697c4
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-06-06 23:38:40 +0530

    BINARY SEARCH TREE

--- DAY__85/BINARYTREE_LowestCommonAncestor.java
+++ DAY__85/BINARYTREE_LowestCommonAncestor.java
@@ -1,5 +1,8 @@
 package DataStructuresAlgorithm.DAY__85;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class BINARYTREE_LowestCommonAncestor {
     static class Node{
         int data;
@@ -27,22 +30,59 @@ public class BINARYTREE_LowestCommonAncestor {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-        ancestor(root,4,5,0,0);
+        System.out.println(lca(root,4,7).data);
+        System.out.println(lca2(root,4,7).data);
     }
-    public static void ancestor(Node root,int n1,int n2,int n1a,int n2a){
+    public static Node lca(Node root, int n1 , int n2){
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1 ,path1);
+        getPath(root,n2,path2);
+        //last common ancestor
+        int i=0;
+        for(;i<path1.size() && i<path2.size() ; i++){
+            if(path1.get(i) != path2.get(i)){
+                break;
+            }
+        }
+        //last equal node -> i-1th
+        Node lca = path1.get(i-1);
+        return lca;
+    }
+    public static boolean getPath(Node root,int n,ArrayList<Node> path){
         if(root == null){
-            return;
+            return false;
         }
-        ancestor(root.left,n1,n2,n1a,n2a);
-        if(root.data == n1){
-            n1a = root.data;
+        path.add(root);
+        if(root.data == n){
+            return true;
         }
-        ancestor(root.right,n1,n2,n1a,n2a);
-        if(root.data == n1a){
-            System.out.println(n1a);
-            return;
+        boolean foundLeft = getPath(root.left, n ,path);
+        boolean foundRight = getPath(root.right,n,path);
+
+        if(foundRight || foundLeft){
+            return true;
+        }
+        path.remove(path.size()-1);
+        return false;
+    }
+    //Approach 2
+    public static Node lca2(Node root, int n1, int n2){
+        if(root == null || root.data==n1 || root.data==n2){
+            return root;
         }
 
+        Node leftLca = lca2(root.left,n1,n2);
+        Node rightLca = lca2(root.right,n1,n2);
 
+        //leftLca=valid  rightLca=Null
+        if(rightLca == null){
+            return leftLca;
+        }
+        if(leftLca == null){
+            return rightLca;
+        }
+        return root;
     }
 }
