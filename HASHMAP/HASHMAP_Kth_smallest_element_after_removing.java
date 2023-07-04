65b64fcaa12a86a7677bd6d9003097f8a0cb36fb
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-07-05 01:53:15 +0530

    HashMap Questions

---
+++ HASHMAP/HASHMAP_Kth_smallest_element_after_removing.java
@@ -0,0 +1,16 @@
+package DataStructuresAlgorithm.HASHMAP;
+import java.util.TreeSet;
+
+public class HASHMAP_Kth_smallest_element_after_removing {
+    public static void main(String[] args) {
+        int arr[] = {1, 3};
+        int k = 4;
+        TreeSet<Integer> ts =new TreeSet<>();
+        for(int i=1 ; i<=20 ; i++){
+            ts.add(i);
+        }
+        for(int i=0){
+
+        }
+    }
+}
