fc279cb2e50d876a0d0d29b136009345d0ceebd6
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-07-12 00:45:31 +0530

    Graph Revision

---
+++ GRAPHS/GRAPH_REVISION.java
@@ -0,0 +1,20 @@
+package DataStructuresAlgorithm.GRAPHS;
+
+import java.util.ArrayList;
+
+public class GRAPH_REVISION {
+    public class Edge{
+        int src;
+        int dest;
+        Edge(int scr, int dest){
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+    public static void createGraph(ArrayList<Integer> graph){
+
+    }
+    public static void dfs(){
+
+    }
+}
