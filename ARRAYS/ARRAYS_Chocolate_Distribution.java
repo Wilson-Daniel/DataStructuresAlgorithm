bb077ae3c0a1de9f3e35f584287fc3399dca70f6
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-07-16 23:52:48 +0530

    Arrays

--- ARRAYS/ARRAYS_Chocolate_Distribution.java
+++ ARRAYS/ARRAYS_Chocolate_Distribution.java
@@ -5,13 +5,23 @@ import java.util.Arrays;
 public class ARRAYS_Chocolate_Distribution {
     public static void main(String[] args) {
         int[] chocolatePack = {12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50};
-        int k=7;
+        int m=7;
+        //edge case
+//        System.out.println(maxDif);
+//        if(m==0 || chocolatePack.length==0){
+//            return -1;
+//        }
+//        if(m>chocolatePack.length){
+//            return -1;
+//        }
+        int minDif = Integer.MAX_VALUE;
         Arrays.sort(chocolatePack);
-        int maxDif = Integer.MAX_VALUE;
-        for(int i=0 ; i<=chocolatePack.length-k ; i++){
-            int diff = chocolatePack[i+k-1]-chocolatePack[i];
-            maxDif = Math.min(maxDif,diff);
+        for(int i=0  ;i<=chocolatePack.length-m ; i++){
+            int currDif = chocolatePack[i+m-1]-chocolatePack[i];
+            if(currDif<minDif){
+                minDif = currDif ;
+            }
         }
-        System.out.println(maxDif);
+        System.out.println(minDif);
     }
 }
