d71ae6365b97dcf24a219eef1da88a5b6487d9cc
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-07-19 02:57:47 +0530

    Priority Queue

--- PRIORITY_QUEUE/PRIORITYQUEUE_Implementation.java
+++ PRIORITY_QUEUE/PRIORITYQUEUE_Implementation.java
@@ -17,19 +17,68 @@ public class PRIORITYQUEUE_Implementation {
         }
     }
     public static void main(String[] args) {
-        PriorityQueue<Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
+//        PriorityQueue<Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
+////        pq.add(3);//O(logn)
+////        pq.add(5);
+////        pq.add(2);
+////        pq.add(1);
+//        pq.add(new Student("A",1));
+//        pq.add(new Student("B",5));
+//        pq.add(new Student("C",2));
+//        pq.add(new Student("D",12));
+//        while(!pq.isEmpty()){
+//            System.out.println(pq.peek().name +" -> "+pq.peek().rank);//O(1)
+//            pq.remove();   //logn
+//        }
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
 //        pq.add(3);//O(logn)
 //        pq.add(5);
 //        pq.add(2);
 //        pq.add(1);
-        pq.add(new Student("A",1));
-        pq.add(new Student("B",5));
-        pq.add(new Student("C",2));
-        pq.add(new Student("D",12));
+        pq.add(new Pair(5));
+        pq.add(new Pair(54));
+        pq.add(new Pair(548));
+        pq.add(new Pair(60));
+
+//        while(!pq.isEmpty()){
+//            System.out.println(pq.peek().num);//O(1)
+//            pq.remove();   //logn
+//        }
+        String[] arr = {"5","548","546","60"};
+        System.out.println(printLargest(arr));
+
+    }
+    static String printLargest(String[] arr) {
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>();
+        for(int i=0 ; i<arr.length ; i++){
+            pq.add(new Pair(Integer.parseInt(arr[i])));
+        }
+        String ans = "";
+
         while(!pq.isEmpty()){
-            System.out.println(pq.peek().name +" -> "+pq.peek().rank);//O(1)
-            pq.remove();   //logn
+            System.out.println(pq.peek().num);
+            ans+=Integer.toString(pq.remove().num);
         }
 
+        return ans;
+        // code here
+    }
+    public static class Pair implements Comparable<Pair>{
+        int num;
+        Pair(int num){
+            this.num = num;
+        }
+        @Override
+        public int compareTo(Pair s2){
+            String a = Integer.toString(this.num)+Integer.toString(s2.num);
+            String b = Integer.toString(s2.num)+Integer.toString(this.num);
+            int ai = Integer.parseInt(a);
+            int bi = Integer.parseInt(b);
+            if(ai<bi){
+                return ai-bi;
+            }else{
+                return bi-ai;//6054854654
+            }
+        }
     }
 }
