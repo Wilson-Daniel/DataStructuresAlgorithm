9e10802447fe31add2c7b9563e76579e658e1387
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-07-28 02:34:27 +0530

    Recursion Revision

---
+++ RECURSION/RECURSION_Fibonacci_Number.java
@@ -0,0 +1,14 @@
+package DataStructuresAlgorithm.RECURSION;
+
+public class RECURSION_Fibonacci_Number {
+    public static void main(String[] args) {
+        System.out.println(fib(6));
+    }
+    public static int fib(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+
+        return fib(n-1)+fib(n-2);
+    }
+}
