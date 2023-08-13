ece6d4a73c9f4dc44e7d5fe6a24bb884089a40b7
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-08-14 03:29:06 +0530

    OOPS

---
+++ OOPS/OOPS.java
@@ -0,0 +1,22 @@
+package DataStructuresAlgorithm.OOPS;
+
+
+public class OOPS {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+    }
+}
+class Pen{
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+    void setTip(int tip){
+        this.tip = tip;
+    }
+}
+
