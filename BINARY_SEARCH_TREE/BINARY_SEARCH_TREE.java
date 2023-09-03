af78137733ec603f5377169e978a71e40fe0a250
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-09-03 18:42:03 +0530

    BINARY SEARCH

--- BINARY_SEARCH_TREE/BINARY_SEARCH_TREE.java
+++ BINARY_SEARCH_TREE/BINARY_SEARCH_TREE.java
@@ -32,6 +32,8 @@ public class BINARY_SEARCH_TREE {
             root =  insert(root,value[i]);
         }
         inOrder(root);
+        System.out.println();
+        System.out.println(search(root,7));
     }
     public static void inOrder(Node root){
         if(root==null){
@@ -40,5 +42,53 @@ public class BINARY_SEARCH_TREE {
         inOrder(root.left);
         System.out.print(root.data+" ");
         inOrder(root.right);
+    }
+    public static boolean search(Node root,int key){ //O(n)
+        if(root==null){
+            return false;
+        }
+        if(root.data == key){
+            return true;
+        }
+        if (root.data > key) {
+            return search(root.left,key);
+        }else{
+            return search(root.right,key);
+        }
+    }
+    public static Node delete(Node root,int val){
+        if(root==null){
+            return null;
+        }
+        //step1 - search
+        if(root.data < val){
+            root.right = delete(root.right,val);
+        }else if(root.data > val){
+            root.left = delete(root.left,val);
+        }else{
+            //voila mill gta
+            //case-1 leaf child
+            if(root.left == null && root.right==null){
+                return null;
+            }
+            //case-2 One child
+            if(root.left==null){
+                return root.right;
+            }else if(root.right==null){
+                return root.left;
+            }
+            //case-3
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right,IS.data);
+
+        }
+        return root;
+    }
+    public static Node findInorderSuccessor(Node root){
+        while(root.left != null){
+            root=root.left;
+        }
+        return root;
     }
 }
