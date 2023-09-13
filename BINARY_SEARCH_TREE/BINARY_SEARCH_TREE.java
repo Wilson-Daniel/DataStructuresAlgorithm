857efbe78365858b0aae726bda49501aa36f3071
Wilson-Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-09-13 23:25:02 +0530

    BINARY SEARCH

--- BINARY_SEARCH_TREE/BINARY_SEARCH_TREE.java
+++ BINARY_SEARCH_TREE/BINARY_SEARCH_TREE.java
@@ -49,40 +49,52 @@ public class BINARY_SEARCH_TREE {
         System.out.println();
         ArrayList<Integer> ans = new ArrayList<>();
         rootToLeaf(root,ans);
+        printInRange(root,2,6);
+        System.out.println();
+        System.out.println(isValidBST(root,null,null));
+        createMirror(root);
+        preOrder(root);
+        System.out.println();
+        int[] sortedArr = {3,5,6,8,10,11,12};
+        Node Sroot = sortedArrayToBST(sortedArr,0,sortedArr.length-1);
+        preOrder(Sroot);
+        balanceBST(root);
+        System.out.println();
+        preOrder(root);
     }
-    public static boolean search(Node root,int val){
+    public static boolean search(Node root,int key){
         if(root==null){
             return false;
         }
-        if(root.data==val){
+        if(root.data==key){
             return true;
         }
-        if(root.data>val){
-            return search(root.left,val);
+        if(root.data>key){
+            return search(root.left,key);
         }else{
-            return search(root.right,val);
+            return search(root.right,key);
         }
     }
     public static Node delete(Node root,int val){
         if(root==null){
             return null;
         }
-        if(root.data > val){
-            root.left = delete(root.left,val);
-        }else if(root.data < val){
+        if(root.data<val){
             root.right = delete(root.right,val);
+        }else if(root.data>val){
+            root.left = delete(root.left,val);
         }else{
             if(root.left==null && root.right==null){
                 return null;
             }
             if(root.left==null){
                 return root.right;
-            }else if(root.right==null){
+            }else if(root.right == null){
                 return root.left;
             }else{
-                Node IS = findInOrderSuccessor(root.right);
+                Node IS = findInOrderSuccessor(root);
                 root.data = IS.data;
-                root.right = delete(root,IS.data);
+                root.right = delete(root.right,val);
             }
         }
         return root;
@@ -93,17 +105,28 @@ public class BINARY_SEARCH_TREE {
         }
         return root;
     }
-    public static void rootToLeaf(Node root, ArrayList<Integer> path){
+    public static void rootToLeaf(Node root,ArrayList<Integer> path){
         if(root==null){
             return;
         }
         path.add(root.data);
-        if(root.left==null && root.right==null){
+        if(root.left==null && root.right== null){
             printPath(path);
         }
         rootToLeaf(root.left,path);
         rootToLeaf(root.right,path);
         path.remove(path.size()-1);
+    }
+
+    public static Node createMirror(Node root){
+        if(root==null){
+            return null;
+        }
+        Node leftMirror = createMirror(root.left);
+        Node rightMirror = createMirror(root.right);
+        root.left = rightMirror;
+        root.right = leftMirror;
+        return root;
     }
     public static void printPath(ArrayList<Integer> ans){
         for(int i: ans){
@@ -111,4 +134,68 @@ public class BINARY_SEARCH_TREE {
         }
         System.out.println();
     }
+    public static void printInRange(Node root,int k1,int k2){
+        if(root==null){
+            return;
+        }
+        if(root.data>=k1 && root.data<=k2){
+            printInRange(root.left,k1,k2);
+            System.out.print(root.data+" ");
+            printInRange(root.right,k1,k2);
+        }else if(root.data>k1){
+            printInRange(root.left,k1,k2);
+        }else{
+            printInRange(root.right,k1,k2);
+        }
+    }
+    public static boolean isValidBST(Node root,Node min,Node max){
+        if(root==null){
+            return true;
+        }
+        if(min!=null && root.data<min.data){
+            return false;
+        }
+        if(max!=null && root.data>max.data){
+            return false;
+        }
+        return isValidBST(root.left,min,root) && isValidBST(root.right,root,max);
+    }
+    //Sorted Array to BST
+    public static Node sortedArrayToBST(int[] arr,int st,int end){
+        if(st>end){
+            return null;
+        }
+        int mid = (st+end)/2;
+        Node root = new Node(arr[mid]);
+        root.left = sortedArrayToBST(arr,st,mid-1);
+        root.right = sortedArrayToBST(arr,mid+1,end);
+        return root;
+    }
+    //BST To Balanced BST
+    public static Node createBST(ArrayList<Integer> inorder,int st,int end){
+        if(st>end){
+            return null;
+        }
+        int mid = (st+end)/2;
+        Node root = new Node(inorder.get(mid));
+        root.left = createBST(inorder,st,mid-1);
+        root.right = createBST(inorder,mid+1,end);
+        return root;
+    }
+    public static Node balanceBST(Node root){
+        ArrayList<Integer> inorder = new ArrayList<>();
+        getInOrder(root,inorder);
+        root = createBST(inorder,0,inorder.size()-1);
+        return root;
+    }
+
+    public static void getInOrder(Node root,ArrayList<Integer> inorder){
+        if(root==null){
+            return;
+        }
+        getInOrder(root.left,inorder);
+        inorder.add(root.data);
+        getInOrder(root.right,inorder);
+    }
+
 }
---
+++ BINARY_SEARCH_TREE/BINARY_SEARCH_TREE_SortedArrayToBST.java
@@ -0,0 +1,9 @@
+package DataStructuresAlgorithm.BINARY_SEARCH_TREE;
+
+public class BINARY_SEARCH_TREE_SortedArrayToBST {
+    public static void main(String[] args) {
+        int[] arr = {3,5,6,8,10,11,12};
+
+    }
+
+}
