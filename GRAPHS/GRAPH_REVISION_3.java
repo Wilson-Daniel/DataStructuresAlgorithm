7a882c344b4e285f9bc16a607f602eafdb973e48
Wilson Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2023-11-07 01:33:09 +0530

    GRAPHS

--- GRAPHS/GRAPH_REVISION_3.java
+++ GRAPHS/GRAPH_REVISION_3.java
@@ -1,10 +1,7 @@
 package DataStructuresAlgorithm.GRAPHS;
 
 import java.sql.Array;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class GRAPH_REVISION_3 {
     static class Edge{
@@ -30,19 +27,21 @@ public class GRAPH_REVISION_3 {
             graph1[i] = new ArrayList<>();
         }
         graph1[0].add(new Edge2(0,1));
-        //graph[0].add(new Edge(0,2));
-        graph1[0].add(new Edge2(0,3));
+        graph1[0].add(new Edge2(0,2));
+//        graph1[0].add(new Edge2(0,3));
 
         graph1[1].add(new Edge2(1,0));
-        graph1[1].add(new Edge2(1,2));
+        graph1[1].add(new Edge2(1,3));
 
 //        graph1[2].add(new Edge2(2,0));
-        graph1[2].add(new Edge2(2,1));
+        graph1[2].add(new Edge2(2,0));
+        graph1[2].add(new Edge2(2,4));
 
-        graph1[3].add(new Edge2(3,0));
-        graph1[3].add(new Edge2(3,4));
+        graph1[3].add(new Edge2(3,1));
+//        graph1[3].add(new Edge2(3,4));
 
-        graph1[4].add(new Edge2(4,3));
+//        graph1[4].add(new Edge2(4,3));
+        graph1[4].add(new Edge2(4,2));
     }
 
     public static void createGraph(ArrayList<Edge> graph[]){
@@ -108,6 +107,65 @@ public class GRAPH_REVISION_3 {
         }
     }
 
+    //------------------CYCLE DETECTION----------
+    public static boolean detectCycle(ArrayList<Edge2>[] graph){
+        boolean[] vis = new boolean[graph.length];
+        for(int i=0 ; i<graph.length ; i++){
+            if(!vis[i]) {
+                if (detectCycleUtil(graph, vis, i, -1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean detectCycleUtil(ArrayList<Edge2>[] graph, boolean[] vis,int curr,int par){
+        vis[curr] = true;
+        for(int i=0 ; i<graph[curr].size() ; i++){
+            Edge2 e = graph[curr].get(i);
+            //case 1
+            if(!vis[e.dest]){
+                if(detectCycleUtil(graph,vis,e.dest,curr)){
+                    return true;
+                }
+            }//case 2
+            else if(vis[e.dest] && e.dest!=par){
+                return true;
+            }
+        }
+        return false;
+    }
+    //bipartite Graph
+    static boolean check = true;
+
+    public static boolean isBipartite(ArrayList<Edge2>[] graph) {
+        int col[] = new int[graph.length];
+        for (int i = 0; i < col.length; i++) {
+            col[i] = -1;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < graph.length; i++) {
+            if (col[i] == -1) {
+                q.add(i);
+                col[i] = 0;
+                while (!q.isEmpty()) {
+                    int curr = q.remove();
+                    for (int j = 0; j < graph[curr].size(); j++) {
+                        Edge2 e = graph[curr].get(j);
+                        if (col[e.dest] == -1) {
+                            int nextCol = col[curr] == 0 ? 1 : 0;
+                            col[e.dest] = nextCol;
+                            q.add(e.dest);
+                        } else if (col[e.dest] == col[curr]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         int V = 5;
         ArrayList<Edge>[] graph = new ArrayList[V];
@@ -119,34 +177,10 @@ public class GRAPH_REVISION_3 {
         boolean[] vis = new boolean[graph2.length];
         createGraph2(graph2);
         System.out.println(detectCycle(graph2));
-    }
-
-    //------------------CYCLE DETECTION----------
-    public static boolean detectCycle(ArrayList<Edge2>[] graph){
-        boolean[] vis = new boolean[graph.length];
-        for(int i=0 ; i<graph.length ; i++){
-            if(!vis[i]){
-                if(detectCycleUtil(graph,i,-1,vis)){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public static boolean detectCycleUtil(ArrayList<Edge2>[] graph, int curr,int par,boolean[] vis){
-        vis[curr]=true;
-        for(int i=0 ; i<graph[curr].size() ; i++){
-            Edge2 e = graph[curr].get(i);
-            if(!vis[e.dest]){
-                if(detectCycleUtil(graph,e.dest,curr,vis)){
-                    return true;
-                }
-            }else if(vis[e.dest] && e.dest!=par) {
-                return true;
-            }
-        }
-        return false;
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(graph[0].get(0).src);
+        Set<Integer> set2 = new HashSet<>();
+        System.out.println(isBipartite(graph2));
     }
 
 
